2ab1ec20dabec214e6bab60f78143c57d13100b5
jayanigerine <jayanigerine@gmail.com>
2020-09-29 23:12:56 -0400

    first commit

---
+++ src/main/java/ufm/cis/jayani/SpringDependencyExample.java
@@ -0,0 +1,14 @@
+package ufm.cis.jayani;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ufm.cis.jayani.di.House;
+
+public class SpringDependencyExample {
+    public static void main(String[] args) {
+
+        ApplicationContext appContext = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+
+        House house = (House) appContext.getBean("house");
+        house.doSomething();
+    }
+}
